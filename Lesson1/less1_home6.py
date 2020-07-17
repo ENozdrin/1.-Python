685071de045a96a9c1528482ec4120f33d9f36b4
Enozdrin <nozdrin.em@gmail.com>
2020-07-17 14:39:18 +0300

    commit_master

--- Lesson1/less1_home6.py
+++ Lesson1/less1_home6.py
@@ -1,9 +1,8 @@
 a = int(input('Введите значение a (км): '))
 b = int(input('Введите значение b (км): '))
 day = 1
-while  a < b:
-     a = a + 0.1 * a
-     day = day + 1
+while a < b:
+    a = a + 0.1 * a
+    day = day + 1
 print(f'Номер дня, на который результат спортсмена составит не менее b км = {day}')
 
-# some text
